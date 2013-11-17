06166fbd8d13a6af8438e35a1a3134d80a412990
unknown <mtlcon@mtlcon.suk.sas.com>
2013-11-17 15:51:55 +0000

    Add Ubuntu dev machine configuration

---
+++ ubuntu-dev/bootstrap.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get -y install git
+apt-get -y install subversion
+mkdir /tmp/vagrant
+chown -R vagrant /tmp/vagrant
+
+#java
+wget --output-document=/tmp/vagrant/jdk-6u45-linux-x64.bin https://jacderida-software.s3.amazonaws.com/jdk-6u45-linux-x64.bin
+chmod a+x /tmp/vagrant/jdk-6u45-linux-x64.bin
+/tmp/vagrant/jdk-6u45-linux-x64.bin
+mv ./jdk1.6.0_45 ./java-6-oracle
+mkdir /usr/lib/jvm
+mv ./java-6-oracle /usr/lib/jvm
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/java-6-oracle/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/java-6-oracle/bin/javac" 1
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/java-6-oracle/bin/javaws" 1
+update-alternatives --config java
+update-alternatives --config javac
+update-alternatives --config javaws
+echo "JAVA_HOME=/usr/lib/jvm/java-6-oracle" >> /etc/profile
+echo "PATH=$PATH:$HOME/bin:$JAVA_HOME/bin" >> /etc/profile
+echo "export JAVA_HOME" >> /etc/profile
+echo "export JAVA_BIN" >> /etc/profile
+echo "export PATH" >> /etc/profile
+#virtualbox
+wget --output-document=/tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb http://download.virtualbox.org/virtualbox/4.2.18/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
+dpkg -i /tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
+# vagrant
+wget --output-document=/tmp/vagrant/vagrant_1.3.5_x86_64.deb http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/vagrant_1.3.5_x86_64.deb
+dpkg -i /tmp/vagrant/vagrant_1.3.5_x86_64.deb
+# chrome
+apt-get -y install libxss1
+wget --output-document=/tmp/vagrant/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i /tmp/vagrant/google-chrome-stable_current_amd64.deb
+rm -r /tmp/vagrant
