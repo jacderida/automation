26134ea57c1c2a7150cf0984109e3f3bab38a5ce
Chris O'Neil <chris.oneil@gmail.com>
2013-11-18 22:38:33 +0000

    Install RabbitVCS

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -10,6 +10,8 @@ function install_apt_packages {
 	apt-get -y install git
 	apt-get -y install subversion
 	apt-get -y install vim-gnome
+	add-apt-repository -y ppa:rabbitvcs/ppa && apt-get update
+	apt-get -y install rabbitvcs-core rabbitvcs-nautilus3 rabbitvcs-cli
 }
 
 function install_oracle_jdk_6 {
