189b1b5ac8f301a946637fb496f8f4b6c9bbe736
Chris O'Neil <chris.oneil@gmail.com>
2013-11-18 22:15:39 +0000

    Add proxy settings to /etc/environment

--- provisioning/ubuntudev/proxy.sh
+++ provisioning/ubuntudev/proxy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 export http_proxy=$1
+export https_proxy=http://$1/
+export HTTP_PROXY=$1
+export HTTPS_PROXY=http://$1/
 if [ ! -e /etc/apt/apt.conf.d/80proxy ]; then
 	echo "Acquire::http::proxy \"http://$http_proxy/\";" >> /etc/apt/apt.conf.d/30proxy
 
@@ -16,4 +19,13 @@ if [ ! -e /etc/apt/apt.conf.d/80proxy ]; then
 
 	echo "export http_proxy=$1" >> /etc/profile.d/setproxy.sh
 	chmod +x /etc/profile.d/setproxy.sh
+
+	echo "http_proxy=\"http://$1/\"" >> /etc/environment
+	echo "https_proxy=\"http://$1/\"" >> /etc/environment
+	echo "ftp_proxy=\"http://$1/\"" >> /etc/environment
+	echo "no_proxy=\"localhost,127.0.0.1,localaddress,.localdomain.com\"" >> /etc/environment
+	echo "HTTP_PROXY=\"http://$1/\"" >> /etc/environment
+	echo "HTTPS_PROXY=\"http://$1/\"" >> /etc/environment
+	echo "FTP_PROXY=\"http://$1/\"" >> /etc/environment
+	echo "NO_PROXY=\"localhost,127.0.0.1,localaddress,.localdomain.com\"" >> /etc/environment
 fi
