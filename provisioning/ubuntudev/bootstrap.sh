f0548e321af821d1eb96f93996e221c2557ce8ce
Chris O'Neil <chris.oneil@gmail.com>
2013-11-18 13:41:53 +0000

    Idempotency for vagrant

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -51,8 +51,10 @@ function install_virtualbox {
 }
 
 function install_vagrant {
-	wget --output-document=/tmp/vagrant/vagrant_1.3.5_x86_64.deb https://jacderida-software.s3.amazonaws.com/vagrant_1.3.5_x86_64.deb
-	dpkg -i /tmp/vagrant/vagrant_1.3.5_x86_64.deb
+	if [ ! -e /opt/vagrant/bin/vagrant ]; then
+		wget --output-document=/tmp/vagrant/vagrant_1.3.5_x86_64.deb https://jacderida-software.s3.amazonaws.com/vagrant_1.3.5_x86_64.deb
+		dpkg -i /tmp/vagrant/vagrant_1.3.5_x86_64.deb
+	fi
 }
 
 function install_chrome {
