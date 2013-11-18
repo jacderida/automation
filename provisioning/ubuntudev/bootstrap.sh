847f5123646d09dd17352a1439cb3259e5a58251
Chris O'Neil <chris.oneil@gmail.com>
2013-11-18 13:40:39 +0000

    Idempotency for VirtualBox

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -44,8 +44,10 @@ function install_oracle_jdk_7 {
 }
 
 function install_virtualbox {
-	wget --output-document=/tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb https://jacderida-software.s3.amazonaws.com/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
-	dpkg -i /tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
+	if [ ! -e /usr/bin/virtualbox ]; then
+		wget --output-document=/tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb https://jacderida-software.s3.amazonaws.com/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
+		dpkg -i /tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
+	fi
 }
 
 function install_vagrant {
