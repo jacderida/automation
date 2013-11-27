a03bef5211420409a240a85d6ef9e0221f420613
Chris O'Neil <chris.oneil@gmail.com>
2013-11-27 11:48:31 +0000

    Set environment vars for Install4j

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -79,6 +79,10 @@ function install_install4j {
 	if [ ! -e /opt/install4j/bin/install4j ]; then
 		wget --output-document=/tmp/vagrant/install4j_linux_5_1_7.deb https://jacderida-software.s3.amazonaws.com/install4j_linux_5_1_7.deb
 		dpkg -i /tmp/vagrant/install4j_linux_5_1_7.deb
+		echo "export INSTALL4J_HOME_V5_1=/opt/install4j5" >> ~/install4j_env.sh
+		echo "export INSTALL4J_HOME_V5_1_1=/opt/install4j5" >> ~/install4j_env.sh
+		chmod a+x ~/install4j_env.sh
+		mv ~/install4j_env.sh /etc/profile.d/
 	fi
 }
 
