20947eb6c4374f33a6ee2ecbbcab3a68e011c2da
Chris O'Neil <chris.oneil@gmail.com>
2013-12-03 17:02:12 +0000

    Add LXC plugin for vagrant

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -59,6 +59,7 @@ function install_vagrant {
 	if [ ! -e /opt/vagrant/bin/vagrant ]; then
 		wget --output-document=/tmp/vagrant/vagrant_1.3.5_x86_64.deb https://jacderida-software.s3.amazonaws.com/vagrant_1.3.5_x86_64.deb
 		dpkg -i /tmp/vagrant/vagrant_1.3.5_x86_64.deb
+		vagrant plugin install vagrant-lxc
 	fi
 }
 
