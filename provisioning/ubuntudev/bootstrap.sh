0105e64b0a777e8c4e004813b609e4004ffcdcac
Chris O'Neil <chris.oneil@gmail.com>
2013-12-27 21:17:40 +0000

    Apparently curl isn't installed by default

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -13,6 +13,7 @@ function install_apt_packages {
 	apt-get -y install vim-gnome
 	add-apt-repository -y ppa:rabbitvcs/ppa && apt-get update
 	apt-get -y --force-yes install rabbitvcs-core rabbitvcs-nautilus3 rabbitvcs-cli
+	apt-get -y install curl
 }
 
 function install_oracle_jdk_6 {
