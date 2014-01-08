5184d2686752bf8f446bafadd523a31f29a25bf4
Chris O'Neil <chris.oneil@gmail.com>
2014-01-08 20:55:14 +0000

    Provision install4j in Vagrantfile

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -18,18 +18,6 @@ function install_apt_packages {
 	apt-get -y install xchat
 }
 
-function install_install4j {
-	if [ ! -e /opt/install4j/bin/install4j ]; then
-		wget --output-document=/tmp/vagrant/install4j_linux_5_1_7.deb https://jacderida-software.s3.amazonaws.com/install4j_linux_5_1_7.deb
-		dpkg -i /tmp/vagrant/install4j_linux_5_1_7.deb
-		echo "export INSTALL4J_HOME_V5_1=/opt/install4j5" >> ~/install4j_env.sh
-		echo "export INSTALL4J_HOME_V5_1_1=/opt/install4j5" >> ~/install4j_env.sh
-		chmod a+x ~/install4j_env.sh
-		mv ~/install4j_env.sh /etc/profile.d/
-	fi
-}
-
 initial_setup
 install_apt_packages
-install_install4j
 rm -r /tmp/vagrant
