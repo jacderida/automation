91325ea71f25ad7fd15cb8f2762ca07008ae0c7c
Chris O'Neil <chris.oneil@gmail.com>
2014-01-08 13:22:49 +0000

    Remove java setup from main bootstrap

--- provisioning/ubuntudev/bootstrap.sh
+++ provisioning/ubuntudev/bootstrap.sh
@@ -18,37 +18,6 @@ function install_apt_packages {
 	apt-get -y install xchat
 }
 
-function install_oracle_jdk_6 {
-	if [ ! -e /usr/lib/jvm/java-6-oracle/bin/java ]; then
-		wget --output-document=/tmp/vagrant/jdk-6u45-linux-x64.bin https://jacderida-software.s3.amazonaws.com/jdk-6u45-linux-x64.bin
-		chmod a+x /tmp/vagrant/jdk-6u45-linux-x64.bin
-		/tmp/vagrant/jdk-6u45-linux-x64.bin
-		mv ./jdk1.6.0_45 ./java-6-oracle
-		mkdir /usr/lib/jvm
-		mv ./java-6-oracle /usr/lib/jvm
-		update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/java-6-oracle/bin/java" 1
-		update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/java-6-oracle/bin/javac" 1
-		update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/java-6-oracle/bin/javaws" 1
-		update-alternatives --config java
-		update-alternatives --config javac
-		update-alternatives --config javaws
-		echo "JAVA_HOME=/usr/lib/jvm/java-6-oracle" >> /etc/profile
-		echo "PATH=$PATH:$HOME/bin:$JAVA_HOME/bin" >> /etc/profile
-		echo "export JAVA_HOME" >> /etc/profile
-		echo "export JAVA_BIN" >> /etc/profile
-		echo "export PATH" >> /etc/profile
-	fi
-}
-
-function install_oracle_jdk_7 {
-	if [ ! -e /usr/lib/jvm/java-7-oracle/bin/java ]; then
-		wget --output-document=/tmp/vagrant/jdk-7u45-linux-x64.gz https://jacderida-software.s3.amazonaws.com/jdk-7u45-linux-x64.gz
-		tar -xvf /tmp/vagrant/jdk-7u45-linux-x64.gz
-		mv ./jdk1.7.0_45 ./java-7-oracle/
-		mv ./java-7-oracle /usr/lib/jvm/
-	fi
-}
-
 function install_virtualbox {
 	if [ ! -e /usr/bin/virtualbox ]; then
 		wget --output-document=/tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb https://jacderida-software.s3.amazonaws.com/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
@@ -93,8 +62,6 @@ function install_install4j {
 
 initial_setup
 install_apt_packages
-install_oracle_jdk_6
-install_oracle_jdk_7
 install_virtualbox
 install_vagrant
 install_chrome
