8a4606ab1c1987e58b59be8b8e760ea1c989a111
Chris O'Neil <chris.oneil@gmail.com>
2014-01-08 13:20:31 +0000

    Add scripts to setup java 6 and 7

---
+++ sh/oracle_jdk_6.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ ! -e /usr/lib/jvm/java-6-oracle/bin/java ]; then
+	wget --output-document=/tmp/vagrant/jdk-6u45-linux-x64.bin https://jacderida-software.s3.amazonaws.com/jdk-6u45-linux-x64.bin
+	chmod a+x /tmp/vagrant/jdk-6u45-linux-x64.bin
+	/tmp/vagrant/jdk-6u45-linux-x64.bin
+	mv ./jdk1.6.0_45 ./java-6-oracle
+	mkdir /usr/lib/jvm
+	mv ./java-6-oracle /usr/lib/jvm
+	update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/java-6-oracle/bin/java" 1
+	update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/java-6-oracle/bin/javac" 1
+	update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/java-6-oracle/bin/javaws" 1
+	update-alternatives --config java
+	update-alternatives --config javac
+	update-alternatives --config javaws
+	echo "JAVA_HOME=/usr/lib/jvm/java-6-oracle" >> /etc/profile
+	echo "PATH=$PATH:$HOME/bin:$JAVA_HOME/bin" >> /etc/profile
+	echo "export JAVA_HOME" >> /etc/profile
+	echo "export JAVA_BIN" >> /etc/profile
+	echo "export PATH" >> /etc/profile
+fi
