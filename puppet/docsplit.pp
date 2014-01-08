f285dda809fae8a6bc58185614da54ee80567e31
Chris O'Neil <chris.oneil@gmail.com>
2014-01-08 13:30:54 +0000

    Add puppet manifests from previous repo

---
+++ puppet/docsplit.pp
@@ -0,0 +1,24 @@
+group { "puppet":
+	ensure => present,
+}
+
+package { 'docsplit':
+	ensure		=> installed,
+	provider	=> gem,
+}
+
+exec { 'Install GraphicsMagick':
+	command => '/usr/bin/aptitude -y install graphicsmagick',
+}
+
+exec { 'Install Poppler':
+	command => '/usr/bin/apt-get -y install poppler-utils poppler-data' 
+}
+
+exec { 'Install Tesseract':
+	command => '/usr/bin/aptitude -y install tesseract'
+}
+
+exec { 'Install pdftk':
+	command => '/usr/bin/aptitude -y install pdftk'
+}
