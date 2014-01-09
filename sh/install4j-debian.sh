4ac482eb9fe51b4aaa75e95c327e5aec4f4890e6
Chris O'Neil <chris.oneil@gmail.com>
2014-01-09 22:33:10 +0000

    Correct idempotency for Install4j

--- sh/install4j-debian.sh
+++ sh/install4j-debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -e /opt/install4j/bin/install4j ]; then
+if [ ! -e /opt/install4j5 ]; then
 	wget --output-document=/tmp/vagrant/install4j_linux_5_1_7.deb https://jacderida-software.s3.amazonaws.com/install4j_linux_5_1_7.deb
 	dpkg -i /tmp/vagrant/install4j_linux_5_1_7.deb
 	echo "export INSTALL4J_HOME_V5_1=/opt/install4j5" >> ~/install4j_env.sh
