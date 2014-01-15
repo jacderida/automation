fddb92fbef6cb0f59e3043816c7a1a187bb6852e
Chris O'Neil <chris.oneil@gmail.com>
2014-01-15 20:56:59 +0000

    Add python sphinx for generating aws documentation

--- provisioning/dev-java-ubuntu-x64/bootstrap.sh
+++ provisioning/dev-java-ubuntu-x64/bootstrap.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-function initial_setup {
-	apt-get update
-	mkdir /tmp/vagrant
-	chown -R vagrant /tmp/vagrant
+function initial_setup
+{
+    apt-get update
+    mkdir /tmp/vagrant
+    chown -R vagrant /tmp/vagrant
 }
 
-function install_apt_packages {
-	apt-get -y install python-pip
-	apt-get -y install git
-	apt-get -y install subversion
-	apt-get -y --force-yes install git-svn
-	apt-get -y install vim-gnome
-	add-apt-repository -y ppa:rabbitvcs/ppa && apt-get update
-	apt-get -y --force-yes install rabbitvcs-core rabbitvcs-nautilus3 rabbitvcs-cli
-	apt-get -y install curl
-	apt-get -y install xchat
+function install_apt_packages
+{
+    apt-get -y install python-pip
+    apt-get -y install git
+    apt-get -y install subversion
+    apt-get -y --force-yes install git-svn
+    apt-get -y install vim-gnome
+    add-apt-repository -y ppa:rabbitvcs/ppa && apt-get update
+    apt-get -y --force-yes install rabbitvcs-core rabbitvcs-nautilus3 rabbitvcs-cli
+    apt-get -y install curl
+    apt-get -y install xchat
+    apt-get -y install python-sphinx
 }
 
 initial_setup
