c4de0fa1c788611f176cb604f6f87f784f890ff3
Chris O'Neil <chris.oneil@gmail.com>
2014-03-12 11:38:04 +0000

    Refactor tmux script to source dependencies

--- sh/tmux-debian.sh
+++ sh/tmux-debian.sh
@@ -1,49 +1,16 @@
 #!/usr/bin/env bash
 
-function install_lib_event
-{
-    if [ ! -f "/usr/local/lib/libevent.la" ]; then
-        wget --output-document=/tmp/libevent-2.0.21-stable.tar.gz https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
-        tar -xvf /tmp/libevent-2.0.21-stable.tar.gz -C /tmp
-        present_directory=`pwd`
-        cd /tmp/libevent-2.0.21-stable
-        ./configure && make
-        make install
-        cd $present_directory
-        rm /tmp/libevent-2.0.21-stable.tar.gz
-        rm -r -f /tmp/libevent-2.0.21-stable
-    fi
-}
+source ./ncurses-5.9.sh
+source ./libevent-2.0.21-stable.sh
 
-function install_ncurses
-{
-    if [ ! -f "/usr/bin/tic" ]; then
-        wget --output-document=/tmp/ncurses-5.9.tar.gz http://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
-        tar -zxf /tmp/ncurses-5.9.tar.gz -C /tmp
-        present_directory=`pwd`
-        cd /tmp/ncurses-5.9
-        ./configure --prefix=/usr
-        make && make install
-        cd $present_directory
-        rm -r -f /tmp/ncurses-5.9
-    fi
-}
-
-function install_tmux
-{
-    if [ ! -f "/usr/local/bin/tmux" ]; then
-        wget --output-document=/tmp/tmux-1.8.tar.gz http://downloads.sourceforge.net/tmux/tmux-1.8.tar.gz
-        tar -xvf /tmp/tmux-1.8.tar.gz -C /tmp
-        present_directory=`pwd`
-        cd /tmp/tmux-1.8
-        ./configure && make
-        make install
-        cd $present_directory
-        rm /tmp/tmux-1.8.tar.gz 
-        rm -r -f /tmp/tmux-1.8
-    fi
-}
-
-install_ncurses
-install_lib_event
-install_tmux
+if [ ! -f "/usr/local/bin/tmux" ]; then
+    wget --output-document=/tmp/tmux-1.8.tar.gz http://downloads.sourceforge.net/tmux/tmux-1.8.tar.gz
+    tar -xvf /tmp/tmux-1.8.tar.gz -C /tmp
+    present_directory=`pwd`
+    cd /tmp/tmux-1.8
+    ./configure && make
+    make install
+    cd $present_directory
+    rm /tmp/tmux-1.8.tar.gz 
+    rm -r -f /tmp/tmux-1.8
+fi
