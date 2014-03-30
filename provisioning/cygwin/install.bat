b02d7060481d58868334f768a4f49dcf31342707
Chris O'Neil <chris.oneil@gmail.com>
2014-03-30 16:28:52 +0100

    Add the installation script for cygwin

---
+++ provisioning/cygwin/install.bat
@@ -0,0 +1,9 @@
+mkdir c:\cygwin
+mkdir c:\cygwin\packages
+setup-x86_64.exe ^
+--quiet-mode ^
+--disable-buggy-antivirus ^
+--root c:\cygwin ^
+--local-package-dir c:\cygwin\packages ^
+--site http://mirrors-uk.go-parts.com/cygwin ^
+--packages binutils,curl,gcc,git,libuuid-devel,openssh,python,python-crypto,python-paramiko,vim,wget
