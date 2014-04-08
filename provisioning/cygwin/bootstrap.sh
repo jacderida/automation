277d4caf87dba409e5d91a8225fd071f155a2980
Chris O'Neil <chris.oneil@gmail.com>
2014-04-08 16:55:22 +0100

    Rename awk to ack!

--- provisioning/cygwin/bootstrap.sh
+++ provisioning/cygwin/bootstrap.sh
@@ -5,4 +5,4 @@ source ../../sh/python_easy_install.sh
 source ../../sh/install_pip.sh
 source ../../sh/install_build_sphinx.sh
 source ../../sh/tmux-1.9a.sh
-source ../../sh/awk-2.12.sh
+source ../../sh/ack-2.12.sh
