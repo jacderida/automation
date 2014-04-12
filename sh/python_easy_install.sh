364be015f865976b31a97083de96c83ed74e2dce
Chris O'Neil <chris.oneil@gmail.com>
2014-04-12 23:48:06 +0100

    Use double bracket method
    Regexes don't work with the single bracket method.

--- sh/python_easy_install.sh
+++ sh/python_easy_install.sh
@@ -2,11 +2,11 @@
 
 base_command="wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - |"
 
-if [ ! -e "/usr/bin/easy_install" ]; then
+if [[ ! -e "/usr/bin/easy_install" ]]; then
     present_directory=$(pwd)
     cd /tmp
     wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O ez_setup.py
-    if [ $(uname -s) == CYGWIN_NT-* ]
+    if [[ $(uname -s) == CYGWIN_NT-* ]]
     then
         python ez_setup.py
     else
