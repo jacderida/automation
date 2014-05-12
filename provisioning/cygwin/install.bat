ec855c62c00b6aa3d8549234c3ec890ba371f198
Chris O'Neil <chris.oneil@gmail.com>
2014-05-12 13:51:48 +0100

    Add libarchive-devel package to cygwin setup
    Vagrant now seems to require this to be installed.

--- provisioning/cygwin/install.bat
+++ provisioning/cygwin/install.bat
@@ -1,10 +1,9 @@
 mkdir c:\cygwin
 mkdir c:\cygwin\packages
 setup-x86_64.exe ^
---proxy 172.29.212.87:80 ^
 --quiet-mode ^
 --disable-buggy-antivirus ^
 --root c:\cygwin ^
 --local-package-dir c:\cygwin\packages ^
 --site http://mirrors-uk.go-parts.com/cygwin ^
---packages automake1.14,binutils,cmake,curl,dos2unix,gcc-core,gcc-g++,git,libcrypt-devel,libevent-devel,libncursesw-devel,libuuid-devel,make,openssh,python,python-crypto,python-paramiko,ruby,subversion,util-linux,vim,wget
+--packages automake1.14,binutils,cmake,curl,dos2unix,gcc-core,gcc-g++,git,libarchive-devel,libcrypt-devel,libevent-devel,libncursesw-devel,libuuid-devel,make,openssh,python,python-crypto,python-paramiko,ruby,subversion,util-linux,vim,wget
