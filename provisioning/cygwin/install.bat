03f7833f70b9ddf916e3418a151f0cf52885d7f9
Chris O'Neil <chris.oneil@gmail.com>
2014-05-12 13:57:30 +0100

    bsdtar instead of libarchive-devel

--- provisioning/cygwin/install.bat
+++ provisioning/cygwin/install.bat
@@ -6,4 +6,4 @@ setup-x86_64.exe ^
 --root c:\cygwin ^
 --local-package-dir c:\cygwin\packages ^
 --site http://mirrors-uk.go-parts.com/cygwin ^
---packages automake1.14,binutils,cmake,curl,dos2unix,gcc-core,gcc-g++,git,libarchive-devel,libcrypt-devel,libevent-devel,libncursesw-devel,libuuid-devel,make,openssh,python,python-crypto,python-paramiko,ruby,subversion,util-linux,vim,wget
+--packages automake1.14,binutils,bsdtar,cmake,curl,dos2unix,gcc-core,gcc-g++,git,libcrypt-devel,libevent-devel,libncursesw-devel,libuuid-devel,make,openssh,python,python-crypto,python-paramiko,ruby,subversion,util-linux,vim,wget
