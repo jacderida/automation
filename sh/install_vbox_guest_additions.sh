e4cb0abd152d3ac53aa631e70c25f6c1c04c4ce0
Chris O'Neil <chris.oneil@gmail.com>
2014-06-08 23:53:41 +0100

    Install vbox guest additions

---
+++ sh/install_vbox_guest_additions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo -e "\nInstalling virtualbox guest additions..."
+VBOX_VERSION=$(cat /tmp/.vbox_version)
+cd /tmp
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -f VBoxGuestAdditions_$VBOX_VERSION.iso
