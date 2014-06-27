3cf492d4d3ece328254e359f9ef9a077fef9833a
Chris O'Neil <chris.oneil@gmail.com>
2014-06-27 16:03:47 +0100

    Put the version number in the box name

--- box_templates/postgres-CentOS-6.5-x86_64/build.sh
+++ box_templates/postgres-CentOS-6.5-x86_64/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 packer build template.json
-mv packer_virtualbox-iso_virtualbox.box postgres-CentOS-6.5-x86_64.box
-s3cmd put postgres-CentOS-6.5-x86_64.box s3://jacderida-vagrant-boxes/postgres-CentOS-6.5-x86_64.box
+mv packer_virtualbox-iso_virtualbox.box postgres_9.3.4-CentOS-6.5-x86_64.box
+s3cmd put postgres_9.3.4-CentOS-6.5-x86_64.box s3://jacderida-vagrant-boxes/postgres_9.3.4-CentOS-6.5-x86_64.box
 rm -rf packer_cache
