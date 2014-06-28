768a7df0e077552d3653bf83c5310239d756de02
Chris O'Neil <chris.oneil@gmail.com>
2014-06-28 23:50:28 +0100

    Update virtualbox script to use latest version

--- sh/virtualbox-debian.sh
+++ sh/virtualbox-debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ ! -e /usr/bin/virtualbox ]; then
-	wget --output-document=/tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb https://jacderida-software.s3.amazonaws.com/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
-	dpkg -i /tmp/vagrant/virtualbox-4.2_4.2.18-88780~Ubuntu~precise_amd64.deb
+    wget --output-document=/tmp/virtualbox-4.3_4.3.12-93733~Ubuntu~raring_amd64.deb http://download.virtualbox.org/virtualbox/4.3.12/virtualbox-4.3_4.3.12-93733~Ubuntu~raring_amd64.deb
+	dpkg -i /tmp/virtualbox-4.3_4.3.12-93733~Ubuntu~raring_amd64.deb
 fi
