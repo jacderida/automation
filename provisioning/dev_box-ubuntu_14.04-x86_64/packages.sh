361ceee69eb484b48357f6fa41af1b3498954603
Chris O'Neil <chris.oneil@gmail.com>
2014-06-28 23:32:01 +0100

    Configuration to provision hardware based Ubuntu

---
+++ provisioning/dev_box-ubuntu_14.04-x86_64/packages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get -y install python-pip
+apt-get -y install git
+apt-get -y install subversion
+apt-get -y --force-yes install git-svn
+add-apt-repository -y ppa:rabbitvcs/ppa && apt-get update
+apt-get -y --force-yes install rabbitvcs-core rabbitvcs-nautilus3 rabbitvcs-cli
+apt-get -y install curl
+apt-get -y install xchat
+apt-get -y install python-sphinx
+apt-get -y install dos2unix
+apt-get -y install gnome-shell
+apt-get -y install exuberant-ctags
+apt-get -y install libxml-xpath-perl
+apt-get -y install libxml2-dev
+apt-get -y install libxslt1-dev
+apt-get -y install zlib1g-dev
+apt-get -y install libncurses5-dev
+apt-get -y install libevent-dev
+apt-get -y install cmake
+apt-get -y install smbfs
+apt-get -y install tree
---
+++ provisioning/dev_box-ubuntu_14.04-x86_64/provision.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source ./packages.sh
+source ../../sh/oracle_jdk_6.sh
+source ../../sh/oracle_jdk_7.sh
+source ../../sh/virtualbox-debian.sh
+source ../../sh/vagrant-debian.sh
+source ../../sh/tmux-1.9a.sh
+source ../../sh/chrome-debian.sh
+source ../../sh/netbeans.sh
+source ../../sh/install4j-debian.sh
+source ../../sh/maven3.0.5.sh
+source ../../sh/lynx2.8.7.sh
+source ../../sh/s3cmd.sh
+source ../../sh/ack-2.12.sh
+source ../../sh/vim-7.4-debian.sh
