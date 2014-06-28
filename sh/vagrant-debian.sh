8c90704dd9604955a8bbeab3eb40b5aa09377b2d
Chris O'Neil <chris.oneil@gmail.com>
2014-06-29 00:05:34 +0100

    Change vagrant script to use the latest version

--- sh/vagrant-debian.sh
+++ sh/vagrant-debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ ! -e /opt/vagrant/bin/vagrant ]; then
-	wget --output-document=/tmp/vagrant/vagrant_1.3.5_x86_64.deb https://jacderida-software.s3.amazonaws.com/vagrant_1.3.5_x86_64.deb
-	dpkg -i /tmp/vagrant/vagrant_1.3.5_x86_64.deb
+	wget --output-document=/tmp/vagrant_1.6.3_x86_64.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb
+	dpkg -i /tmp/vagrant_1.6.3_x86_64.deb
 fi
