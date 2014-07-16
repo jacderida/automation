d9de812bcd6385e737525cc1d3160be2e498b98c
Chris O'Neil <chris.oneil@gmail.com>
2014-07-16 17:15:22 +0100

    Uncomment download step

--- sh/eclipse-4.1.4.sh
+++ sh/eclipse-4.1.4.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -d "/opt/eclipse-4.1.4" ]; then
-	#wget --output-document=/tmp/eclipse-standard-luna-R-linux-gtk-x86_64.tar.gz https://jacderida-software.s3.amazonaws.com/eclipse-standard-luna-R-linux-gtk-x86_64.tar.gz
+	wget --output-document=/tmp/eclipse-standard-luna-R-linux-gtk-x86_64.tar.gz https://jacderida-software.s3.amazonaws.com/eclipse-standard-luna-R-linux-gtk-x86_64.tar.gz
 	tar -xvf /tmp/eclipse-standard-luna-R-linux-gtk-x86_64.tar.gz -C /tmp/
     cp -r /tmp/eclipse /opt/eclipse-4.1.4
     rm -rf /tmp/eclipse
