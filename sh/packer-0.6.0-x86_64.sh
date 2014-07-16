38bd0f79f0cc979694e2aeafe1178e1229ed934d
Chris O'Neil <chris.oneil@gmail.com>
2014-07-16 17:29:46 +0100

    Add packer script

---
+++ sh/packer-0.6.0-x86_64.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ ! -d "/opt/packer-0.6.0" ]]; then
+	wget --output-document=/tmp/packer-0.6.0_linux_amd64.zip https://dl.bintray.com/mitchellh/packer/0.6.0_linux_amd64.zip
+    mkdir /opt/packer-0.6.0
+    mkdir /tmp/packer
+	tar -xvf /tmp/packer-0.6.0_linux_amd64.zip -C /tmp/packer
+    cp -r /tmp/packer /opt/packer-0.6.0
+    rm -rf /tmp/packer
+    rm /tmp/packer-0.6.0_linux_amd64.zip
+fi
