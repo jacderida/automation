e7900ce7048225066e65086966ed622b67492cb6
Chris O'Neil <chris.oneil@gmail.com>
2014-07-17 11:20:46 +0100

    Upload box to S3 on completion

--- box_templates/base_server-CentOS-6.5-x86_64/build.sh
+++ box_templates/base_server-CentOS-6.5-x86_64/build.sh
@@ -1,3 +1,4 @@
 packer build template.json
 mv packer_virtualbox-iso_virtualbox.box base_server-CentOS-6.5-x86_64.box
-rmdir /S /Q packer_cache
+aws s3 base_server-CentOS-6.5-x86_64.box s3://jacderida-vagrant-boxes/base_server-CentOS-6.5-x86_64.box --acl public-read
+rm -rf packer_cache
