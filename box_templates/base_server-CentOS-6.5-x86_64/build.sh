d13d9f07ee52e37ac8a98f903a3b23938a953e40
Chris O'Neil <chris.oneil@gmail.com>
2014-07-21 11:57:58 +0100

    Change to use aws python to upload postgres box

--- box_templates/base_server-CentOS-6.5-x86_64/build.sh
+++ box_templates/base_server-CentOS-6.5-x86_64/build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 packer build template.json
 mv packer_virtualbox-iso_virtualbox.box base_server-CentOS-6.5-x86_64.box
 aws s3 base_server-CentOS-6.5-x86_64.box s3://jacderida-vagrant-boxes/base_server-CentOS-6.5-x86_64.box --acl public-read
--- box_templates/postgres_9.3.4-CentOS_6.5-x86_64/build.sh
+++ box_templates/postgres_9.3.4-CentOS_6.5-x86_64/build.sh
@@ -2,5 +2,5 @@
 
 packer build template.json
 mv packer_virtualbox-iso_virtualbox.box postgres_9.3.4-CentOS-6.5-x86_64.box
-s3cmd put postgres_9.3.4-CentOS-6.5-x86_64.box s3://jacderida-vagrant-boxes/postgres_9.3.4-CentOS-6.5-x86_64.box
+aws s3 cp postgres_9.3.4-CentOS-6.5-x86_64.box s3://jacderida-vagrant-boxes/postgres_9.3.4-CentOS-6.5-x86_64.box --acl public-read
 rm -rf packer_cache
