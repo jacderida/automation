7fbbeb4a3e132a2c92cb161680972e5b09e576dd
Chris O'Neil <chris.oneil@gmail.com>
2014-08-20 16:18:52 +0100

    Add AWS and Rackspace Vagrant plugins

--- sh/vagrant-debian.sh
+++ sh/vagrant-debian.sh
@@ -3,4 +3,8 @@
 if [ ! -e /opt/vagrant/bin/vagrant ]; then
 	wget --output-document=/tmp/vagrant_1.6.3_x86_64.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb
 	dpkg -i /tmp/vagrant_1.6.3_x86_64.deb
+    vagrant plugin install vagrant-aws
+    vagrant box add aws https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+    vagrant plugin install vagrant-rackspace
+    vagrant box add rackspace https://github.com/mitchellh/vagrant-rackspace/raw/master/dummy.box
 fi
