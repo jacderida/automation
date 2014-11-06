561a781d9fdf068d9f54917d0273e628d11fa22a
Chris O'Neil <chris.oneil@gmail.com>
2014-11-06 19:15:48 +0000

    Add docker to dev box

--- provisioning/dev_box-ubuntu_14.04-x86_64/provision.sh
+++ provisioning/dev_box-ubuntu_14.04-x86_64/provision.sh
@@ -15,3 +15,4 @@ source ../../sh/eclipse-4.1.4.sh
 source ../../sh/packer-0.6.0-x86_64.sh
 source ../../sh/jq-1.4.sh
 source ../../sh/mongodb-linux-x86_64-2.6.4.sh
+source ../../sh/docker_latest-debian.sh
