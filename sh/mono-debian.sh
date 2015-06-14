84f26e01da32c17afad3fa70af04add4a7b75268
Chris O'Neil <chris.oneil@gmail.com>
2015-06-14 02:35:45 +0100

    Hack for the MVC 3.0.0.1 security fix
    This fix continues to cause pain. It seems it can be gotten around with
    this manual intervention. Well, at least, this is how to get things to
    build on Mono.

--- sh/mono-debian.sh
+++ sh/mono-debian.sh
@@ -2,3 +2,6 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 apt-get update
 apt-get install -y mono-complete
+# This is for the annoying MVC 3.0.0.1 security fix
+mkdir /usr/lib/mono/gac/System.Web.Mvc/3.0.0.1__31bf3856ad364e35
+wget --output-document=/usr/lib/mono/gac/System.Web.Mvc/3.0.0.1__31bf3856ad364e35/System.Web.Mvc.dll https://jacderida-misc.s3.amazonaws.com/System.Web.Mvc.dll
